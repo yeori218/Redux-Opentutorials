c467d09ae183bfa9848d5724cd2dff5bb0333a37
yeori218 <74519339+yeori218@users.noreply.github.com>
2021-07-07 20:59:40 +0900

    JavaScript File Separation

---
+++ script/without.js
@@ -0,0 +1,41 @@
+function red(){
+    document.querySelector("#red").innerHTML = `
+        <div class="container" id="component_red">
+        <h1>red</h1>
+        <input type="button" value="fire" onclick="
+        document.querySelector('#component_red').style.backgroundColor = 'red';
+        document.querySelector('#component_green').style.backgroundColor = 'red';
+        document.querySelector('#component_blue').style.backgroundColor = 'red';
+        ">
+        </div>
+    `;
+}
+red();
+
+function green(){
+    document.querySelector("#green").innerHTML = `
+        <div class="container" id="component_green">
+        <h1>green</h1>
+        <input type="button" value="fire" onclick="
+        document.querySelector('#component_green').style.backgroundColor = 'green';
+        document.querySelector('#component_red').style.backgroundColor = 'green';
+        document.querySelector('#component_blue').style.backgroundColor = 'green';
+        ">
+        </div>
+    `;
+}
+green();
+
+function blue(){
+    document.querySelector("#blue").innerHTML = `
+        <div class="container" id="component_blue">
+        <h1>blue</h1>
+        <input type="button" value="fire" onclick="
+        document.querySelector('#component_blue').style.backgroundColor = 'blue';
+        document.querySelector('#component_red').style.backgroundColor = 'blue';
+        document.querySelector('#component_green').style.backgroundColor = 'blue';
+        ">
+        </div>
+    `;
+}
+blue();
